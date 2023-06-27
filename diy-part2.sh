eca606abc26be67b772e333d6c5a446cb9009c89
blogj <i@gao4.top>
2023-06-27 10:24:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/g' feeds/luci/collections/luci/Makefile
+sed -i 's/Bootstrap theme/Argonv3 theme/g' feeds/luci/collections/luci/Makefile
